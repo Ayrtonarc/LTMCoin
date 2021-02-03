758fe09da29dc9bf59efcb88e1e2c10cae5b2986
ayrton <ayrton.arc@gmail.com>
2021-02-02 23:23:09 -0800

    17 Minando nuevos Bloques

--- src/service/index.js
+++ src/service/index.js
@@ -8,7 +8,13 @@ const { HTTP_PORT = 3000 } = process.env;
 const app = express();
 const blockchain = new Blockchain();
 
+blockchain.addBlock('express'); //b;oque de prueba
+
 app.use(bodyParser.json());
+
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.blocks);
+});
 
 app.listen(HTTP_PORT, () => {
     console.log(`Service HTTP:${HTTP_PORT} listening...`);
