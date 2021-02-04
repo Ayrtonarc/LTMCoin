4ed424251b89b8f4d19c5ee6ee8f32aa7080b432
ayrton <ayrton.arc@gmail.com>
2021-02-03 22:03:38 -0800

    20 configuracion servicio P2P para conectarse a otros nodos

--- src/service/index.js
+++ src/service/index.js
@@ -8,24 +8,24 @@ const { HTTP_PORT = 3000 } = process.env;
 const app = express();
 const blockchain = new Blockchain();
 
-blockchain.addBlock('express'); //b;oque de prueba
+blockchain.addBlock('express');
 
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
-    res.json(blockchain.blocks);
+  res.json(blockchain.blocks);
 });
 
 app.post('/mine', (req, res) => {
-    const { body: { data } } = req;
-    const block = blockchain.addBlock(data);
+  const { body: { data } } = req;
+  const block = blockchain.addBlock(data);
 
-    res.json({
-        blocks: blockchain.blocks.length,
-        block,
-    });
+  res.json({
+    blocks: blockchain.blocks.length,
+    block,
+  });
 });
 
 app.listen(HTTP_PORT, () => {
-    console.log(`Service HTTP:${HTTP_PORT} listening...`);
+  console.log(`Service HTTP:${HTTP_PORT} listening...`);
 });
